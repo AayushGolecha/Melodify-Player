dce35753e3919649a972dd7a80c556a3cd3d1793
Aayush Golecha <62604015+AayushGolecha@users.noreply.github.com>
2024-05-19 22:40:33 +0530

    updated

--- script.js
+++ script.js
@@ -124,5 +124,15 @@ async function main(){
             document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
         }
     })
+
+    // Add an event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "0"
+    })
+
+    // Add an event listener for close button
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-120%"
+    })
 }
 main()
